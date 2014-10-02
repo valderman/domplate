58aeb0c53abc236f7c32dac2fd0cc8de6267c0a5
Anton Ekblad <anton@ekblad.cc>
2014-10-02 23:34:33 +0100

    Fix the Haddock.

--- Text/Domplate.hs
+++ Text/Domplate.hs
@@ -3,45 +3,48 @@
 --   attributes are stripped from the HTML. The following attributes are
 --   recognized:
 --
---   insert="identifier" - replace the tag's contents with the value bound to
---   `identifier` in the substitution context.
+--   @insert="identifier"@ - replace the tag's contents with the value bound to
+--   @identifier@ in the substitution context.
 --
---   when="identifier" - only render this tag if `identifier` is set to true in
---   the substitution context.
+--   @when="identifier"@ - only render this tag if @identifier@ is set to true
+--   in the substitution context.
 --
---   unless="identifier" - the dual of `when`; only render this tag if
---   `identifier` is set to false in the substitution context.
+--   @unless="identifier"@ - the dual of @when@; only render this tag if
+--   @identifier@ is set to false in the substitution context.
 --
---   forall="identifier" - render this tag and its contents once for each
---   element in the list bound to `identifier` in the substitution context.
+--   @forall="identifier"@ - render this tag and its contents once for each
+--   element in the list bound to @identifier@ in the substitution context.
 --   The contents of the element may refer to the current iteration's value of
---   `identifier` by that same name.
+--   @identifier@ by that same name.
 --
---   Additionally, the special `replace` tag will be replaced by whatever is
---   bound to its `with` attribute: <replace with="identifier">.
+--   Additionally, the special @replace@ tag will be replaced by whatever is
+--   bound to its @with@ attribute: @\<replace with="identifier"\>@.
 --
 --   Contexts can be nested, in which case nested keys are separated by
---   periods, as in `parent.child.grandchild`. Keys may be prefixed with a
+--   periods, as in @parent.child.grandchild@. Keys may be prefixed with a
 --   question mark, in which case they are considered to be "weak keys".
 --   If a weak key does not exist in the context, it will be replaced by a
 --   sensible default value instead of causing an error. The defaults for the
 --   different value types are as follows:
---   * bool: false
---   * text: ""
---   * list: []
---   * map: {}
+--
+--     * bool: false
+--     * text: ""
+--     * list: []
+--     * map: {}
 --
 --   In general, values used as text must be declared text by the context and
 --   so on, but the following coercions are permitted:
---   * bool to string
---   * list to bool
+--
+--     * bool to string
+--     * list to bool
+--
 --   Coercion of list to bool, with the empty list being considered false and
 --   all other lists considered true, is permitted to allow templates to take
 --   special action in the case of an empty list.
 --
 --   While it is highly recommended to build contexts programatically, a parser
 --   for a rather clumsy context file format is included in
---   @Text.Domplate.ContextParser@.
+--   "Text.Domplate.ContextParser".
 module Text.Domplate (
     Monoid (..),
     Template, Context, Value (..), Key,
